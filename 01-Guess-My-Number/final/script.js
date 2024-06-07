9eb4a293a9af4fc2051871aa4218a5296bc34b6f
user.email <jobayer.ahmed109@gmail.com>
2024-06-07 20:29:28 +0600

    Winning bg color changed

--- 01-Guess-My-Number/final/script.js
+++ 01-Guess-My-Number/final/script.js
@@ -10,20 +10,30 @@ const checkBtn = document.querySelector(".check");
 checkBtn.addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
+  // When guessing start
   if (!guess) {
     document.querySelector(".message").textContent = `⛔  No number inserted `;
+
+    // Winning logic
   } else if (guess === secretNum) {
     document.querySelector(".message").textContent = `✌️  Correct number ! `;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    // Too high guessing
   } else if (guess > secretNum) {
     if (score > 1) {
       document.querySelector(".message").textContent = `📈  Too High `;
       score--;
       document.querySelector(".score").textContent = score;
+
+      // Game over
     } else {
-      document.querySelector(".message").textContent = `📉  Too Low `;
-      score--;
-      document.querySelector(".score").textContent = score;
+      document.querySelector(".message").textContent = `💔 Game over `;
+      document.querySelector(".score").textContent = 0;
     }
+
+    // Too low guessing
   } else if (guess < secretNum) {
     document.querySelector(".message").textContent = `📉  Too Low `;
     score--;

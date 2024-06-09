612a1278e8aefea66c9612f91ca00c8d6119a620
user.email <jobayer.ahmed109@gmail.com>
2024-06-09 06:50:17 +0600

    Guess project completely done

--- 01-Guess-My-Number/final/script.js
+++ 01-Guess-My-Number/final/script.js
@@ -1,10 +1,9 @@
 "use strict";
 
 // -------------------------------------------
-const secretNum = Math.floor(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = secretNum;
-
+let secretNum = Math.floor(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 const checkBtn = document.querySelector(".check");
 checkBtn.addEventListener("click", function () {
@@ -17,10 +16,16 @@ checkBtn.addEventListener("click", function () {
     // Winning logic
   } else if (guess === secretNum) {
     document.querySelector(".message").textContent = `✌️  Correct number ! `;
+    document.querySelector(".number").textContent = secretNum;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.borderRadius = "50%";
     document.querySelector(".number").style.backgroundColor = "black";
     document.querySelector(".number").style.color = "white";
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
 
     // Too high guessing
   } else if (guess > secretNum) {
@@ -43,4 +48,15 @@ checkBtn.addEventListener("click", function () {
   }
 });
 
+// ---------------------------------------------------------------
+
 // Again Button handle
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNum = Math.floor(Math.random() * 20) + 1;
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".score").textContent = score;
+});
